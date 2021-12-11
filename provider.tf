5ed0c51af44f7b81d884d2bc72d00d01493f6fc0
Mariam Mashaka <mkavumo@gmail.com>
2021-12-11 18:10:33 -0500

    test

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
   backend "s3" {
       bucket = " " # provide the name of a bucket that exist for the state file
       key = "dev/terraform.state" # a folder called dev will be created and the state file place in it
-      region = " " # the region where the bucket is located 
+      region = "" # the region where the bucket is located 
       # Use DynamoDB for state locking 
       dynamodb_table = " " # name of the dynamodb table to use please make the partition key is set to LockID 
   }
